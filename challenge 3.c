a80777a5e869012539beff4990a17b1a6474bf31
Mazid Mohammed <mazidmohamed47@gmail.com>
2025-09-06 01:03:11 +0100

    Update challenge 3.c

--- challenge 3.c
+++ challenge 3.c
@@ -3,19 +3,19 @@
 
 int main()
 {
-    int N1,N2,S,T;
-    printf("veuillez entrer le nommber 1 :");
-    scanf("%d",&N1);
-    printf("veuillez entrer le nommber 2 :");
-    scanf("%d",&N2);
-    if (N1==N2){
-            S=N1+N2;
-            T=S*3;
-        printf("la resultat est : %d",T);
+    int Nommber1,Nommber2,Somme,Triple;
+    printf("veuillez entrer le Nommber 1 :");
+    scanf("%d",&Nommber1);
+    printf("veuillez entrer le Nommber 2 :");
+    scanf("%d",&Nommber2);
+    if (Nommber1==Nommber2){
+            Somme=Nommber1+Nommber2;
+            Triple=Somme*3;
+        printf("la resultat est : %d",Triple);
     }
     else {
-        S=N1+N2;
-        printf("la resultat est : %d ",S);
+        Somme=Nommber1+Nommber2;
+        printf("la resultat est : %d ",Somme);
     }
 
     return 0;
